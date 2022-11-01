0c0a2887dfa7e254eddff42a14966c1fca75410d
lucasfmonsores <55259849+lucasfmonsores@users.noreply.github.com>
2022-11-01 18:37:50 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# web-platform-dap3vd
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-cz8ai4)
---
+++ js/script1.js
@@ -0,0 +1,72 @@
+
+class Produto {
+
+   constructor() {
+      this.id = 1;
+      this.arrayProdutos = [];
+      
+   }
+   salvar() {
+   let produto = this.lerDados();
+   
+   if(this.validaCampos(produto)) {
+      this.adicionar(produto);
+   }
+
+   
+   
+
+   console.log(this.arrayProdutos);
+   }
+
+   listaTabela() {
+      let tbody = document.getElementById()
+
+      
+   }
+
+   
+
+   adicionar(produto) {
+      this.arrayProdutos.push(produto);
+      this.id++;
+   } 
+
+   lerDados() {
+      let produto = {}
+
+
+      produto.id = this.id;
+      produto.nomeProduto = document.getElementById('produto').value;
+      produto.preco = document.getElementById('preco').value
+
+     return produto;
+   }
+
+   validaCampos(produto) {
+      let msg = '';
+      
+      if(produto.nomeProduto == '') {
+         msg += 'Informe o nome do Produto \n';
+
+   
+      }
+      if(produto.preco == '') {
+         msg += 'Informe o Preco do Produto \n';
+      }
+
+      if(msg != '') {
+         alert(msg);
+         return false
+      }
+      return true;
+   }
+
+   cancelar() {
+
+      
+   }
+}
+
+
+var produto = new Produto()
